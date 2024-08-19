061446d96cfc49d846d3226aa3465a3dd3e1bd7c
Sergio Dobrianskiy <sergio.dobrianskiy@studio.unibo.it>
2024-08-19 23:52:51 +0200

    aggiunta chiusura sessione su logout, tolto back dalla navbar sulle pag cliente, fornitore, admin

--- webapp/src/views/admin/admin_home/admin_home.js
+++ webapp/src/views/admin/admin_home/admin_home.js
@@ -1,21 +1,6 @@
-// import { setNomeUtente } from "../../common/utils.js"
-
-// const utils = require("../../../common/utils.js");
-
 function getUser() {
     fetch('http://localhost:5000/api/user/')
     .then(response => response.json())
     .then(data => setNomeUtente(data[0]))
 }
-
-// function setNomeUtente() {
-//     fetch('http://localhost:5000/api/user/')
-//     .then(response => response.json())
-//     .then(data => {
-//         if (data[0]["nome"] != null) {
-//             $("#salutoUtente").text(`Benvenuto ${data[0]["nome"]}`)
-//         }
-//     })
-// }
-// setNomeUtente();
 
